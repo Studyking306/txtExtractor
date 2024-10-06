4667ee969ca1864ec0611779b61f875e13cc0ef7
Studyking306 <akbartheking7860@gmail.com>
2024-10-06 11:22:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6618274486:AAESKaMTJJ0tOR62pqOPt-a0WcfpTi0y7nk")
-    API_ID = int(os.environ.get("API_ID", "20089019"))
-    API_HASH = os.environ.get("API_HASH", "0b2eb6aa72d5983fa9e4b8c77791c9c4")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "6404676836")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7893216255:AAFsYF2OLaVcrJakxo_tTZWXaqwD6YMs9vo")
+    API_ID = int(os.environ.get("API_ID", "22188044"))
+    API_HASH = os.environ.get("API_HASH", "099e3a1dce52b7677299c3ab8ab3b6ca")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "7080075962")
